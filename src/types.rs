f73a309dca5161131ac3c0bb077255c21dfa439a
Tobias Langer <tobias.langer@conplement.de>
2024-03-18 10:39:22 +0100

    add re-exports of modemmanager-sys types (#4)
    * add re-exports of modemmanager-sys types
    
    * fix formatting for re-exports
    
    * fix consistent feature name for ModemManager-1_20
    
    The feature would be defined as ModemManager-1_20 in the cargo lock file,
    whereas the checks would happen for ModemManager-1_22. This way activating the
    ModemManager-1_20 feature would never have an effect. Change the name to
    ModemManager-1_20 consistently.
    
    * fix: code format: merge use statements

---
+++ src/types.rs
@@ -0,0 +1,19 @@
+/// Provide reexports of modem manager types.
+
+#[cfg(feature = "ModemManager-1_20")]
+pub use modemmanager_sys::{
+    MMBearerAccessTypePreference as BearerAccessTypePreference,
+    MMBearerProfileSource as BearerProfileSource,
+    MMBearerRoamingAllowance as BearerRoamingAllowance,
+};
+pub use modemmanager_sys::{
+    MMBearerAllowedAuth as BearerAllowedAuth, MMBearerApnType as BearerApnType,
+    MMBearerIpFamily as BearerIpFamily, MMBearerIpMethod as BearerIpMethod,
+    MMBearerMultiplexSupport as BearerMultiplexSupport, MMBearerType as BearerType,
+    MMModemAccessTechnology as ModemAccessTechnology, MMModemBand as ModemBand,
+    MMModemCapability as ModemCapability, MMModemCdmaRmProtocol as ModemCdmaRmProtocol,
+    MMModemLock as ModemLock, MMModemMode as ModemMode, MMModemPortType as ModemPortType,
+    MMModemPowerState as ModemPowerState, MMModemState as ModemState,
+    MMModemStateChangeReason as ModemStateChangeReason,
+    MMModemStateFailedReason as ModemStateFailedReason,
+};
